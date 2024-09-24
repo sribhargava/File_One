f551e1ada820b880e392e2b5e5061d08011096d7
Bhargava Srikanth Tatikonda <srikanthbhargava2002@gmail.com>
2024-09-24 14:05:04 +0530

    ATM Project

--- New_File_One.py
+++ New_File_One.py
@@ -1,20 +1,18 @@
-
 # Project on ATM
 
-
-passcode = 6646
-
-def passcode_check():
+PIN = 6646
+def pin_check():
     counter = 0
-
+    print("Welcome to ABC Bank")
     while counter < 3:
-        value = (int(input("Enter Your Passcode: ")))
-
-        if value == passcode:
+        value = (int(input("Enter Your PIN: ")))
+        length = len(str(value))
+        if length == 4:
             print("Your Passcode is Correct")
             return True
         else:
-            print("Entered incorrect passcode, Try Again")
+            print("Entered PIN is incorrect")
+            print("Enter a 4 digit PIN")
             counter = counter + 1
             if counter == 3:
                 print("Max limit is reached, Try again after 30 min.")
@@ -23,7 +21,7 @@ def passcode_check():
 def main():
     acc_balance = 0
     user = True
-    if passcode_check() is True:
+    if pin_check() is True:
         while user is True:
             withdraw_or_deposit = str(input("Would you like to withdraw or deposit (Enter W/D/Cancel)? :"))
             action = withdraw_or_deposit.lower()
@@ -32,20 +30,19 @@ def main():
                 acc_balance += amount
                 print("Your Current Balance is " + str(acc_balance))
                 user = True
-
             if action == "w":
                 amount = int(input("Enter amount to withdraw: "))
-                acc_balance -= amount
-                print("Your Current Balance is " + str(acc_balance))
-                user = True
+                if amount <= acc_balance:
+                    acc_balance -= amount
+                    print("Your Current Balance is " + str(acc_balance))
+                    user = True
+                else:
+                    print("Insufficient Balance")
+                    user = True
 
             if action == "c" or action == "cancel":
                 print("Thank You, Visit again")
                 user = False
-
-
-
-
 main()
 
 
